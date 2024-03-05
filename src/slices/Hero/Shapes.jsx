fd4af9c67cf9d4fa841cde610ddb4390462e6f31
sandrabayabos <sandra.bayabos@gmail.com>
2024-03-05 22:00:32 +0800

    designed geometric shapes for hero

--- src/slices/Hero/Shapes.jsx
+++ src/slices/Hero/Shapes.jsx
@@ -17,6 +17,7 @@ export default function Shapes() {
         camera={{ position: [0, 0, 25], fov: 30, near: 1, far: 40 }}
       >
         <Suspense fallback={null}>
+          <Geometries />
           <ContactShadows
             position={[0, -3.5, 0]}
             opacity={0.65}
@@ -24,9 +25,142 @@ export default function Shapes() {
             blur={1}
             far={9}
           />
-          <Environment preset="studio" />
+          <Environment preset="forest" />
         </Suspense>
       </Canvas>
     </div>
+  );
+}
+
+function Geometries() {
+  const geometries = [
+    {
+      position: [0, 0, 0],
+      r: 0.3,
+      geometry: new THREE.IcosahedronGeometry(3), // gem
+    },
+    {
+      position: [1, -0.75, 4],
+      r: 0.5,
+      geometry: new THREE.CapsuleGeometry(0.5, 1.6, 2, 16), // pill
+    },
+    {
+      position: [-1.4, 2, -4],
+      r: 0.3,
+      geometry: new THREE.DodecahedronGeometry(1.5), // football
+    },
+    {
+      position: [-0.8, -0.75, 5],
+      r: 0.5,
+      geometry: new THREE.TorusGeometry(0.6, 0.25, 16, 32), // Donut
+    },
+    {
+      position: [1.6, 1.6, -4],
+      r: 0.37,
+      geometry: new THREE.OctahedronGeometry(1.5), // Diamond
+    },
+  ];
+
+  const materials = [
+    new THREE.MeshNormalMaterial(),
+    new THREE.MeshStandardMaterial({ color: 0x2ecc71, roughness: 0 }),
+    new THREE.MeshStandardMaterial({ color: 0xf53b57, roughness: 0.4 }),
+    new THREE.MeshStandardMaterial({ color: 0xf78fb3, roughness: 0.2 }),
+    new THREE.MeshStandardMaterial({ color: 0xf7d794, roughness: 0.1 }),
+    new THREE.MeshStandardMaterial({ color: 0x78e08f, roughness: 0.3 }),
+    new THREE.MeshStandardMaterial({
+      color: 0x0c2461,
+      roughness: 0.1,
+      metalness: 0.5,
+    }),
+    new THREE.MeshStandardMaterial({
+      color: 0xfad390,
+      roughness: 0.1,
+      metalness: 0.5,
+    }),
+  ];
+
+  const soundEffects = [
+    new Audio("/sounds/sound1.ogg"),
+    new Audio("/sounds/sound2.ogg"),
+    new Audio("/sounds/sound3.ogg"),
+  ];
+
+  // Pass to Geometry
+  return geometries.map(({ position, r, geometry }) => (
+    <Geometry
+      key={JSON.stringify(position)}
+      position={position.map((p) => p * 2)}
+      geometry={geometry}
+      materials={materials}
+      r={r}
+      soundEffects={soundEffects}
+    />
+  ));
+}
+
+function Geometry({ r, position, geometry, materials, soundEffects }) {
+  const meshRef = useRef();
+  const [visible, setVisible] = useState(false);
+
+  const startingMaterial = getRandomMaterial();
+
+  function getRandomMaterial() {
+    return gsap.utils.random(materials);
+  }
+
+  function handleClick(e) {
+    const mesh = e.object;
+
+    gsap.utils.random(soundEffects).play();
+
+    gsap.to(mesh.rotation, {
+      x: `+=${gsap.utils.random(0, 2)}`,
+      y: `+=${gsap.utils.random(0, 2)}`,
+      z: `+=${gsap.utils.random(0, 2)}`,
+      duration: 1.3,
+      ease: "elastic.out(1, 0.3)",
+      yoyo: true,
+    });
+
+    mesh.materials = getRandomMaterial();
+  }
+
+  const handlePointerOver = () => {
+    document.body.style.cursor = "pointer";
+  };
+
+  const handlePointerOut = () => {
+    document.body.style.cursor = "default";
+  };
+
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      setVisible(true);
+      gsap.from(meshRef.current.scale, {
+        x: 0,
+        y: 0,
+        z: 0,
+        duration: 1,
+        ease: "elastic.out(1, 0.3)",
+        delay: 0.3,
+      });
+    });
+    return () => ctx.revert();
+  }, []);
+
+  return (
+    <group position={position} ref={meshRef}>
+      <Float speed={5 * r} rotationIntensity={6 * r} floatIntensity={5 * r}>
+        <mesh
+          geometry={geometry}
+          material={startingMaterial}
+          onClick={handleClick}
+          onPointerOver={handlePointerOver}
+          onPointerOut={handlePointerOut}
+          visible={visible}
+        />
+      </Float>
+    </group>
   );
 }
